a3d8b74cbe565255eaade5abc8566cc484594797
Алексей Белов <Alenorze@gmail.com>
2017-11-24 17:50:59 +0200

    Models

--- shortener/models.py
+++ shortener/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class LnrzUrl(models.Model):
-    url = models.CharField(max_length=220, ${blank=True, null=True})
+    url = models.CharField(max_length=220)
 
     def __str__(self):
         return str(self.url)
