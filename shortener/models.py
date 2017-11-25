4185e647d96c60c3a599de4b1789a05e20f0406f
Алексей Белов <Alenorze@gmail.com>
2017-11-25 18:37:03 +0200

    Shortener timestamp

--- shortener/models.py
+++ shortener/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class LnrzUrl(models.Model):
     url = models.CharField(max_length=220)
+    shortcode = models.CharField(max_length=15, unique=True)
+    update = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.url)
