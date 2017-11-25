173de7d4127bbca829fd80df2b5a9ef0b5073d7a
Алексей Белов <Alenorze@gmail.com>
2017-11-25 19:15:06 +0200

    Model manager

--- shortener/models.py
+++ shortener/models.py
@@ -3,11 +3,32 @@ from django.db import models
 from .utils import code_generator, create_shortcode
 
 
+
+class LnrzUrlManager(models.Manager):
+    def all(self, *args, **kwargs):
+        qs = super(LnrzUrlManager, self).all(*args, **kwargs)
+        qs = qs_main.filter(active=False)
+        return qs
+    
+    def refresh_shortcodes(self):
+        qs = LnrzUrl.objects.filter(id__gte=1)
+        new_codes = 0
+        for q in qs:
+            q.shortcode = create_shortcode(q)
+            q.save()
+            new_codes += 1
+        return "New codes made: {i}".format(i=new_codes)
+
+
 class LnrzUrl(models.Model):
-    url = models.CharField(max_length=220)
-    shortcode = models.CharField(max_length=15, unique=True)
-    update = models.DateTimeField(auto_now=True)
+    url       = models.CharField(max_length=220)
+    shortcode = models.CharField(max_length=15, default='NO CODE' unique=True, blank=True)
+    update    = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+    active    = models.BooleanField(default=True)
+
+    objects = LnrzUrlManager()
+
 
     def save(self, *args, **kwargs):
         if self.shortcode is None or self.shortcode == '':
