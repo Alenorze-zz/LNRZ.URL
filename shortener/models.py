25d3016ae7bcea56f0ea6aa992c564cf3452cfff
Алексей Белов <Alenorze@gmail.com>
2017-11-25 21:41:38 +0200

    Redirect view

--- shortener/models.py
+++ shortener/models.py
@@ -27,7 +27,7 @@ class LnrzUrlManager(models.Manager):
 
 class LnrzUrl(models.Model):
     url       = models.CharField(max_length=220)
-    shortcode = models.CharField(max_length=SHORTCODE_MAX, unique=True, blank=True)
+    shortcode = models.CharField(max_length=15, unique=True, blank=True)
     update    = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     active    = models.BooleanField(default=True)
--- shortener/views.py
+++ shortener/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import View
 
-def kirr_redirect_view(request, shortcode=None, *args, **kwargs): 
-    return HttpResponse("hello {sc}".format(sc=shortcode))
+from .models import LnrzUrl
+
+
 
 class LnrzCBView(View):
     def get(self, request, shortcode=None, *args, **kwargs):
+        obj = get_object_or_404(LnrzUrl, shortcode=shortcode)
         return HttpResponse("hello again {sc}".format(sc=shortcode))
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse()
+
