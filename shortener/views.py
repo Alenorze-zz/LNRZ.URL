74afa87e4084d89516b425a153c8ea359c4cda54
Алексей Белов <Alenorze@gmail.com>
2017-11-26 17:46:46 +0200

    Shortener template

--- shortener/views.py
+++ shortener/views.py
@@ -6,12 +6,9 @@ from .models import LnrzUrl
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, "home.html", {})
+        return render(request, "shortener/home.html", {})
 
 class LnrzCBView(View): #class based view
     def get(self, request, shortcode=None, *args, **kwargs):
         obj = get_object_or_404(LnrzUrl, shortcode=shortcode)
         return HttpResponseRedirect(obj.url)
-
-    def post(self, request, *args, **kwargs):
-        return HttpResponse()
