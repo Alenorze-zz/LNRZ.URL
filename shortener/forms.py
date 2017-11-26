f246831123dfd2225152eded1a8cbc7717f4c875
Алексей Белов <Alenorze@gmail.com>
2017-11-26 20:57:24 +0200

    Cleaner

--- shortener/forms.py
+++ shortener/forms.py
@@ -3,3 +3,11 @@ from django import forms
 
 class SubmitUrlForm(forms.Form):
     url = forms.CharField(label='Submit URL')
+
+    def clean(self):
+        cleaned_data = super(SubmitUrlForm, self).clean()
+        url = cleaned_data['url']
+    
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        return url
